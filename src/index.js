5c4ffbe1f22d81cfe18fc5475df09eb13ad0e65d
shashi <sasitech18@gmail.com>
2018-01-12 16:49:39 +0000

    Redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
-console.log(store.getState());
+const store = createStore(
+    reducer,
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()                   );
+
+
+//console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
