13de8cadf93d7cdc3391fccae091ed7ca72c4234
shashi <sasitech18@gmail.com>
2018-01-13 16:07:42 +0000

    Calendar grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addRecipe, removeFromCalender } from '../actions';
+import { capitalize } from '../utils/helper';
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
 
 class App extends Component {
-    doThing = () => {
+//    doThing = () => {
         //this.props.dispatch(addRecipe({}))
-        this.props.selectRecipe({})
-    }
+//        this.props.selectRecipe({})
+//    }
 //    state = {
 //        calender: null 
 //    }
@@ -32,9 +34,47 @@ class App extends Component {
 //      this.input.value = ''
 //  }
     render() {
-        console.log('Props', this.props);
+        //console.log('Props', this.props);
+        
+        const { calender, remove } = this.props;
+        const mealOrder = ['breakfast', 'lunch', 'dinner']
+        
         return (
-            <div>Hello World!</div>
+            <div className='container'>
+            <ul className='meal-types'>
+            {mealOrder.map((mealType) => (
+             <li key={mealType} className='subheader'>
+            {capitalize(mealType)}
+            </li>
+           ))}
+         </ul>
+        
+             <div className='calendar'>
+          <div className='days'>
+              {calender.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+           </div>
+            <div className='icon-grid'>
+              {calender.map(({ day, meals }) => (
+                <ul key={day}>
+                  {mealOrder.map((meal) => (
+                    <li key={meal} className='meal'>
+                      {meals[meal]
+                        ? <div className='food-item'>
+                            <img src={meals[meal].image} alt={meals[meal].label}/>
+                            <button onClick={() => remove({meal, day})}>Clear</button>
+                         </div>
+                        : <button className='icon-btn'>
+                            <CalendarIcon size={30}/>
+                          </button>}
+                     </li>
+                 ))}
+                </ul>
+              ))}
+            </div>
+          </div>
+         
+         
+        </div>
 //        <input 
 //            type="text"
 //            ref={(input) => this.input = input}
